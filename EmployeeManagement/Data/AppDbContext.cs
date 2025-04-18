5a2f4850c4f0300d2979d3e52215868fe6efca3f
Charley <charleyann.robertson@gmail.com>
2025-04-18 22:39:09 +0100

    Add employee repository functionality to complete crud actions

--- EmployeeManagement/Data/AppDbContext.cs
+++ EmployeeManagement/Data/AppDbContext.cs
@@ -1,12 +1,16 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using EmployeeManagement.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace EmployeeManagement.Data
 {
     public class AppDbContext : DbContext
     {
+        public DbSet<Employee> Employees { get; set; }
+
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
 
         }
+
     }
 }
