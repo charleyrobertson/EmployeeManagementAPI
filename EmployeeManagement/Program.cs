aeef1cd71db16e1e2197fd1658577918a3dc49b7
Charley <charleyann.robertson@gmail.com>
2025-04-18 22:11:54 +0100

    Add cors

--- EmployeeManagement/Program.cs
+++ EmployeeManagement/Program.cs
@@ -10,8 +10,16 @@ namespace EmployeeManagement
             var builder = WebApplication.CreateBuilder(args);
 
             builder.Services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase("EmployeeDb"));
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("MyCors", builder =>
+                {
+                    builder.WithOrigins("http://localhost:4200").AllowAnyMethod().AllowAnyHeader();
+                });
+            });
 
             var app = builder.Build();
+            app.UseCors("MyCors");
 
             app.MapGet("/", () => "Hello World!");
 
