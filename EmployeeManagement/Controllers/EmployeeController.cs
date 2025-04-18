5b60217738cf6d2c80b55008eaefd973d90e54a8
Charley <charleyann.robertson@gmail.com>
2025-04-18 23:32:34 +0100

    Added api endpoints with validation and swagger ui

--- EmployeeManagement/Controllers/EmployeeController.cs
+++ EmployeeManagement/Controllers/EmployeeController.cs
@@ -15,11 +15,49 @@ namespace EmployeeManagement.Controllers
             _employeeRepository = employeeRepository;
         }
 
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Employee>>> GetAllEmployeesAsync()
+        {
+            return Ok(await _employeeRepository.GetAllAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Employee>> GetEmployeeById(int id)
+        {
+            var employee = await _employeeRepository.GetByIdAsync(id);
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(employee);
+        }
+
         [HttpPost]
         public async Task<ActionResult<Employee>> CreateEmployee(Employee employee) 
         {
             await _employeeRepository.AddEmployeeAsync(employee);
-            return Created();
+            return CreatedAtAction(nameof(GetEmployeeById), new { id = employee.Id }, employee);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteEmployee(int id)
+        {
+            await _employeeRepository.DeleteEmployeeAsync(id);
+            return NoContent();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Employee>> UpdateEmployee(int id, Employee employee)
+        {
+            if(id != employee.Id)
+            {
+                return BadRequest();
+            }
+
+            await _employeeRepository.UpdateEmployeeAsync(employee);
+            return CreatedAtAction(nameof(GetEmployeeById), new { id = employee.Id }, employee);
         }
     }
 }
